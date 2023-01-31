a1517bda1a0f6f83fb52ddd3e06079e9c44b3f67
Anirudh Konidala <kidskodingclub@gmail.com>
2023-01-31 11:47:57 -0600

    MathSolver program

--- CS2/MathSolver/Konidala_Anirudh_MathSolver.java
+++ CS2/MathSolver/Konidala_Anirudh_MathSolver.java
@@ -11,28 +11,25 @@ public class Konidala_Anirudh_MathSolver {
     public static void mathSolver(BufferedReader br, PrintWriter pw) throws IOException {
         while(br.ready()) {
             String s = br.readLine();
-            String operator = s.substring(1, 2);
-            double operand1 = Double.parseDouble(s.substring(0, 1));
-            double operand2 = Double.parseDouble(s.substring(2, 3));
+            String[] contents = s.split(" ");
+            double operand1 = Double.parseDouble(contents[0]);
+            String operator = contents[1];
+            double operand2 = Double.parseDouble(contents[2]);
             switch(operator) {
                 case "+":
-                    pw.write(Integer.toString((int) (operand1 + operand2)));
+                    pw.write(Integer.toString((int) (operand1 + operand2)) + "\n");
                     break;
                 case "-":
-                    pw.write(Integer.toString((int) (operand1 - operand2)));
+                    pw.write(Integer.toString((int) (operand1 - operand2)) + "\n");
                     break;
                 case "*":
-                    pw.write(Integer.toString((int) (operand1 * operand2)));
+                    pw.write(Integer.toString((int) (operand1 * operand2)) + "\n");
                     break;
                 case "/":
-                    if(operand2 % operand1 == 0) {
-                        int temp1 = (int) operand1;
-                        int temp2 = (int) operand2;
-                        pw.write(Integer.toString(temp2 / temp1));
-                    }
-                    else {
-                        pw.write(Double.toString(operand1 / operand2));
-                    }
+                    if((int) (operand1 % operand2) == 0)
+                        pw.write(Integer.toString((int) (operand1 / operand2)) + "\n");
+                    else
+                        pw.write(Double.toString(operand1 / operand2) + "\n");
                     break;
             }    
         }
